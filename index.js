8fdc23ad470f3fd2df3bbe386e4e31aeba5b2435
AbyG1 <abygeorge133@gmail.com>
2024-06-07 18:25:35 +0530

    Add bill section

--- index.js
+++ index.js
@@ -1,4 +1,121 @@
 import menuArray from "./data.js";
+const billSection = document.getElementById('bill')
+const orderArray = []
+
+
+
+document.addEventListener('click',(e) => {
+
+        if(e.target.dataset.add){
+        
+               showBill(+e.target.dataset.add)
+        }
+
+        if(e.target.dataset.remove){
+                removeItems(+e.target.dataset.remove)
+        }
+
+        
+
+
+})
+
+
+function showBill(foodId){
+        
+        billSection.classList.remove('hidden')
+
+        
+        
+        const item = menuArray.find((item) => {
+                
+                return item.id === foodId
+        })
+
+        
+        orderArray.push(item)
+
+        updateMenulist(orderArray)
+       
+        updateTotalPrice(orderArray)
+       
+
+
+
+
+}
+
+
+
+        function updateTotalPrice(orderArray){
+                
+                const totalPrice = orderArray.reduce((total,current)=> {
+
+                        return total + current.price
+                },0)
+         
+                
+              const priceElement = document.getElementById('priceEle')
+              priceElement.textContent = totalPrice
+        }
+
+
+
+        function removeItems(itemToBeRemoved){
+              
+                const itemPosition =  orderArray.findIndex((element)=> {
+                   return element.id === itemToBeRemoved
+                })
+                
+                orderArray.splice(itemPosition,1)
+                
+                updateMenulist(orderArray)
+                updateTotalPrice(orderArray)
+                
+        }
+
+
+
+        function updateMenulist(newOrderArray){
+
+                const orderedItemList = document.getElementById('order-list')
+                if(newOrderArray.length === 0){
+                       
+                        billSection.classList.add('hidden')
+                }
+               
+                        orderedItemList.innerHTML = ''
+                
+                newOrderArray.forEach((item) => {
+
+                        const itemNamePara = document.createElement('p');
+                        const itemPriceEle = document.createElement('span')
+                        const removeEle = document.createElement('span')
+                        
+                        removeEle.dataset.remove = item.id;
+                        removeEle.textContent = 'remove'
+                        itemNamePara.textContent = item.name
+                        itemPriceEle.textContent = `$${item.price}`
+        
+                        
+                        
+                        itemPriceEle.classList.add('align-end')
+                        itemPriceEle.classList.add('item-price')
+                        itemNamePara.classList.add('item-name')
+                        removeEle.classList.add('remove-btn')
+        
+                        itemNamePara.appendChild(itemPriceEle)
+                        itemNamePara.appendChild(removeEle)
+        
+
+                        orderedItemList.appendChild(itemNamePara)
+        
+               })
+
+               
+        }
+
+
 
 
 
@@ -15,9 +132,9 @@ function getContent(){
                                     <p class="item-incredients">${element.ingredients}</p>
                                     <p class="item-price">$${element.price}</p>
                             </div>
-                            <div class="plus-circle">
-                                <i class="fa-solid fa-plus"></i>
-                            </div>
+                            <button class="plus-circle" data-add='${element.id}'>
+                                <i class="fa-solid fa-plus" data-add='${element.id}'></i>
+                            </button>
                     </div>        
                     `
    }).join('')
